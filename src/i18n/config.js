ecea209da33d5288b9b143608ac05bf001d0b7ff
Franciscyber-J <conta002@gmail.com>
2026-04-02 00:50:39 -0300

    Atualização traduções

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,20 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import pt from './locales/pt.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
 
-i18n.use(initReactI18next).init({
-    resources: {
-        pt: { translation: pt },
-        en: { translation: en },
-        es: { translation: es },
-    },
-    lng: 'pt', // fallback language
-    fallbackLng: 'pt',
-    interpolation: {
-        escapeValue: false, // react already safes from xss
-    },
-});
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            pt: { translation: pt },
+            en: { translation: en },
+            es: { translation: es },
+        },
+        fallbackLng: 'pt',
+        supportedLngs: ['pt', 'en', 'es'],
+        load: 'languageOnly', // this ensures 'en-US' becomes 'en'
+        interpolation: {
+            escapeValue: false, // react already safes from xss
+        },
+    });
 
 export default i18n;
